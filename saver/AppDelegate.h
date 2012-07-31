0fa7b0ce029530172050fee30fff29d662ecbfdf
Jon Code <programcodex@gmail.com>
2012-07-30 20:40:17 -0400

    Initial Commit

---
+++ saver/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  saver
+//
+//  Created by Jon Gutwillig on 7/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
