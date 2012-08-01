45c227582e240d920a206128a2fd371ddfa31817
Jon Code <programcodex@gmail.com>
2012-07-31 20:00:21 -0400

    added json framework and goals class

---
+++ saver/Goal.h
@@ -0,0 +1,13 @@
+//
+//  Goal.h
+//  saver
+//
+//  Created by Jon Gutwillig on 7/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Goal : NSObject
+
+@end
